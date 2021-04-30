1177c8597fe57a0e4181b47f37404f84a521fc91
Jordan Petersen <crab8011@gmail.com>
2021-04-29 18:16:37 -0600

    Included sound effects and music. Setup a release build and changed the font of some items.

--- VisualStudio/Commaxian/Commaxian/main.cpp
+++ VisualStudio/Commaxian/Commaxian/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <vector>
 #include <iostream>
 #include <string>
@@ -23,6 +24,8 @@ bool useShaders = false; // False by default... We don't know if shaders are sup
 bool isGameOver = false; // This determines if the specialized game loops should run
 bool isInMenu = true; // This determines if the specialized game loops should run
 bool isPaused = false;
+int bgPos = 0;
+int bgMax = 1919;
 int score = 0; // The temporary score variable
 
 // Key debounce variables
@@ -31,13 +34,30 @@ bool yPress = false, yRelease = true, yTapped = false;
 bool nPress = false, nRelease = true, nTapped = false;
 bool enterPress = false, enterRelease = true, enterTapped = false;
 
+// Volume Values
+const float SFX_VOL = 25.0f;
+const float MUSIC_VOL = 75.0f;
+const float MENU_SFX_VOL = 100.0f;
+
 // Textures and Fonts
 sf::Texture laserTexture;
 sf::Texture texture2;
 sf::Texture texture3;
-sf::Font font;
+sf::Texture bg; // The 'huge' background texture
+sf::Font titleFont;
+sf::Font instructionFont;
+
+// Sounds and Music
+sf::SoundBuffer shootFx;
+sf::Sound shootSound;
+sf::SoundBuffer explodeFx;
+sf::Sound explodeSound;
+sf::SoundBuffer menuSelectFx;
+sf::Sound menuSelectSound;
+sf::Music backgroundMusic;
 
 // Entities
+sf::Sprite background; // The background sprite
 Player player;
 std::vector<Enemy> enemies;
 Laser laser;
@@ -50,11 +70,12 @@ sf::Text gameOverTxt;
 
 // Menu text
 sf::Text titleTxt;
+sf::Text instructionsTxt;
 sf::Text pauseTxt;
 
 // Clocks
 sf::Clock enemyClock; // Used to determine when an enemy updates or moves
-
+sf::Clock bgUpdateClock; // Used to determine when the background moves
 // Shaders
 sf::Shader testShader; // The testShader currently does nothing... It's here for future expansion if I have time to learn GLSL
 
@@ -72,11 +93,46 @@ int main(){
 	if (!laserTexture.loadFromFile("res/textures/laser.png"))
 		std::cout << "Error loading Laser texture" << std::endl;
 
+	if (!bg.loadFromFile("res/textures/bg.png"))
+		std::cout << "Error loading Background texture" << std::endl;
+	bgMax = bg.getSize().y;
+
+	// Load sounds Effects
+	if (!shootFx.loadFromFile("res/sfx/shoot.wav"))
+		std::cout << "Error loading Shoot SFX" << std::endl;
+	shootSound.setBuffer(shootFx);
+	shootSound.setVolume(SFX_VOL);
+
+	if (!menuSelectFx.loadFromFile("res/sfx/select.wav"))
+		std::cout << "Error loading Select SFX" << std::endl;
+	menuSelectSound.setBuffer(menuSelectFx);
+	menuSelectSound.setVolume(MENU_SFX_VOL);
+
+	if (!explodeFx.loadFromFile("res/sfx/explode.wav"))
+		std::cout << "Error loading Explode SFX" << std::endl;
+	explodeSound.setBuffer(explodeFx);
+	explodeSound.setVolume(SFX_VOL);
+
+	
+	
+
+	// Load Background Music
+	if (!backgroundMusic.openFromFile("res/music/a.ogg"))
+		return -1; // error
+
 	// Load fonts
 	//if (!font.loadFromFile("fonts/arial.ttf"))
 	//	std::cout << "Error loading font arial.ttf" << std::endl;
-	if (!font.loadFromFile("res/fonts/SharpRetro.ttf"))
+	if (!titleFont.loadFromFile("res/fonts/SharpRetro.ttf"))
 		std::cout << "Error loading font SharpRetro.ttf" << std::endl;
+
+	if (!instructionFont.loadFromFile("res/fonts/OpenSans-BoldItalic.ttf"))
+		std::cout << "Error loading font OpenSans-BoldItalic.ttf" << std::endl;
+
+	// Create the background
+	background.setPosition(0, 0);
+	background.setTexture(bg);
+	background.setTextureRect(sf::IntRect(0, bgPos, 640, 480)); // start x, start y, width, height
 
 	laser = Laser(player.getLocation(), sf::IntRect(0, 0, 8, 32), laserTexture); // We will only have one laser at a time
 
@@ -101,35 +157,42 @@ int main(){
 
 	// Create the UI
 	//		The score text
-	scoreTxt.setFont(font);
+	scoreTxt.setFont(titleFont);
 	scoreTxt.setString("Score: 000,000,000");
 	scoreTxt.setCharacterSize(25); // px, not pt
 	scoreTxt.setFillColor(sf::Color::White);
 	scoreTxt.setStyle(sf::Text::Italic | sf::Text::Underlined);
 	scoreTxt.setPosition(gridToScreen(13, 0));
 	//		The Attribution text
-	attributionTxt.setFont(font);
+	attributionTxt.setFont(titleFont);
 	attributionTxt.setString("Font: Sharp Retro by JROB774 on OpenGameArt.org");
 	attributionTxt.setCharacterSize(10); // px, not pt
 	attributionTxt.setFillColor(sf::Color::White);
 	attributionTxt.setStyle(sf::Text::Italic | sf::Text::Underlined);
 	attributionTxt.setPosition(gridToScreen(1, 14));
 	//		The Game Over text
-	gameOverTxt.setFont(font);
+	gameOverTxt.setFont(titleFont);
 	gameOverTxt.setString("GAME OVER");
 	gameOverTxt.setCharacterSize(100); // px, not pt
 	gameOverTxt.setFillColor(sf::Color::White);
 	gameOverTxt.setStyle(sf::Text::Italic | sf::Text::Underlined);
 	gameOverTxt.setPosition(window.getSize().x / 6, window.getSize().y / 3);
 	//		The Title Text
-	titleTxt.setFont(font);
+	titleTxt.setFont(titleFont);
 	titleTxt.setString("Commaxian");
 	titleTxt.setCharacterSize(100); // px, not pt
 	titleTxt.setFillColor(sf::Color::White);
 	titleTxt.setStyle(sf::Text::Italic | sf::Text::Underlined);
 	titleTxt.setPosition(window.getSize().x / 6, window.getSize().y / 3);
+	//      The Instructions Text
+	instructionsTxt.setFont(instructionFont);
+	instructionsTxt.setString("Don't let the enemies reach you.\nLeft and Right Arrows to move.\nSpace to shoot.\nEscape to pause.\nPress Enter to Play!");
+	instructionsTxt.setCharacterSize(25); // px, not pt
+	instructionsTxt.setFillColor(sf::Color::White);
+	instructionsTxt.setStyle(sf::Text::Italic | sf::Text::Underlined);
+	instructionsTxt.setPosition(window.getSize().x / 6, 7 * window.getSize().y / 12);
 	//		The Pause Text
-	pauseTxt.setFont(font);
+	pauseTxt.setFont(instructionFont);
 	pauseTxt.setString("Quit? Y/N");
 	pauseTxt.setCharacterSize(50); // px, not pt
 	pauseTxt.setFillColor(sf::Color::White);
@@ -144,7 +207,11 @@ int main(){
 			testShader.setUniform("texture", sf::Shader::CurrentTexture);
 	}
 
+	bgUpdateClock.restart(); // Restart the background update clock
 	enemyClock.restart(); // Restart the clock
+	backgroundMusic.setLoop(true); // Loops the playback of the bgm
+	backgroundMusic.setVolume(MUSIC_VOL); // Sets the bgm volume
+	backgroundMusic.play(); // Starts playing the bgm
 	gameLoop(window);
 	return 0;
 }
@@ -156,6 +223,19 @@ void gameLoop(sf::RenderWindow& window) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+		}
+
+		// Update the background
+		if (bgUpdateClock.getElapsedTime().asMilliseconds() > 50) {
+			bgUpdateClock.restart();
+			if (bgPos < bgMax) {
+				bgPos++;
+			}
+			else {
+				bgPos = 0;
+			}
+
+			background.setTextureRect(sf::IntRect(0, bgPos, 640, 480)); // start x, start y, width, height
 		}
 
 		if (!isGameOver && !isInMenu && !isPaused) {
@@ -171,6 +251,7 @@ void gameLoop(sf::RenderWindow& window) {
 							laser.kill();
 							//enemies.erase(enemies.begin() + j); // We can erase the enemies
 							enemies[j].getSprite().setPosition(gridToScreen(1 + rand() % 18, 2)); // Or provide infinite gameplay
+							explodeSound.play(); // Play explosion sound
 							score++; // Add 1 to the score
 							break; // We hit an enemy. No need to continue, so break out of loop
 						}
@@ -209,6 +290,7 @@ void getInput(sf::RenderWindow& window) {
 	player.handleInput();
 
 
+	/*-----------------Key Debounce-------------------*/
 	// Idea: Use pairs of booleans to manage the state of a key
 	// Read: Debounce
 	// Then use the booleans to perform actions
@@ -256,10 +338,11 @@ void getInput(sf::RenderWindow& window) {
 		enterRelease = true;
 	}
 
+	/*-----------------Key Actions-------------------*/
 
 	if (!isInMenu && !isGameOver && !isPaused) {
-		// If the escape key is pressed, for now just close the window
 		if (escapeTapped) {
+			menuSelectSound.play(); // Play Menu Select sound
 			isPaused = true;
 			escapeTapped = false;
 		}
@@ -269,6 +352,7 @@ void getInput(sf::RenderWindow& window) {
 			// Create a new laser at the player position, then fire
 			if (!laser.isAlive()) { // But only if a laser does not exist.
 				laser.getSprite().setPosition(player.getSprite().getPosition());
+				shootSound.play(); // Play Laser Shoot sound
 				// run it
 				laserThread.launch(); // Fire the laser
 			}
@@ -278,6 +362,7 @@ void getInput(sf::RenderWindow& window) {
 	if (isGameOver && !isInMenu && !isPaused) {
 		if (yTapped) {
 			yTapped = false;
+			menuSelectSound.play(); // Play Menu Select sound
 			quit(window);
 		}
 	}
@@ -287,11 +372,13 @@ void getInput(sf::RenderWindow& window) {
 		// If in main menu, Hit escape to quit
 		if (escapeTapped) {
 			escapeTapped = false;
+			menuSelectSound.play(); // Play Menu Select sound
 			quit(window);
 		}
 		// Hit enter to start the game
 		if (enterTapped) {
 			enemyClock.restart();
+			menuSelectSound.play(); // Play Menu Select sound
 			isInMenu = false;
 		}
 	}
@@ -300,11 +387,13 @@ void getInput(sf::RenderWindow& window) {
 	if (isPaused && !isInMenu && !isGameOver) {
 		if (yTapped) {
 			yTapped = false;
+			menuSelectSound.play(); // Play Menu Select sound
 			quit(window);
 		}
 		if (nTapped) {
 			nTapped = false;
 			isPaused = false;
+			menuSelectSound.play(); // Play Menu Select sound
 		}
 	}
 
@@ -332,6 +421,8 @@ sf::Vector2f gridToScreen(int x, int y) {
 
 // Draw things on the screen
 void renderItems(sf::RenderWindow& window) {
+	// Draw the background first
+	window.draw(background);
 	// Render with shaders, if supported
 	if (useShaders) {
 		if (!isGameOver && !isInMenu) { // If the game isn't over, draw the entities
@@ -359,6 +450,7 @@ void renderItems(sf::RenderWindow& window) {
 
 	if (isInMenu && !isPaused) {
 		window.draw(titleTxt);
+		window.draw(instructionsTxt);
 	}
 
 	if (isPaused && !isInMenu) {
